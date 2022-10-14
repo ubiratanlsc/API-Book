dd3d83a339c55a6cd20a3483791c5c244318d106
Ubiratan Lima <69194011+ubiratanlsc@users.noreply.github.com>
2022-10-14 11:43:49 -0300

    exclusão senha db

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://humanos:super765@node-express-talentos.utuggn6.mongodb.net/express")//?retryWrites=true&w=majority
+mongoose.connect("")//?retryWrites=true&w=majority
 
 
 let db = mongoose.connection;
